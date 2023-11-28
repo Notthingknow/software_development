40a493c8a923c894f3537bec29586ab9c9718595
Carlos <65497066+Ttggg5@users.noreply.github.com>
2023-11-28 20:46:30 +0800

    Add files via upload

---
+++ course_registration_system.sql
@@ -0,0 +1,227 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- 主機： 127.0.0.1
+-- 產生時間： 2023-11-25 18:41:29
+-- 伺服器版本： 10.4.27-MariaDB
+-- PHP 版本： 8.2.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- 資料庫： `course_registration_system`
+--
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `all_course`
+--
+
+CREATE TABLE `all_course` (
+  `id` int(11) NOT NULL,
+  `code` int(4) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `credit` int(1) NOT NULL,
+  `opening_class` varchar(20) NOT NULL,
+  `time` varchar(30) NOT NULL,
+  `classroom` varchar(30) NOT NULL,
+  `initial_quota` int(5) NOT NULL,
+  `number_of_vacancies` int(5) NOT NULL,
+  `teacher_id` char(8) NOT NULL,
+  `outlined` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- 傾印資料表的資料 `all_course`
+--
+
+INSERT INTO `all_course` (`id`, `code`, `name`, `credit`, `opening_class`, `time`, `classroom`, `initial_quota`, `number_of_vacancies`, `teacher_id`, `outlined`) VALUES
+(1, 1001, '作業系統', 3, '資訊三乙', '(一)3', '資電102', 80, 4, '9001', '本課程旨在介紹電腦作業系統的內部組成與運作原理，也就是電腦系統內部管理的相關知識。課程內容包括作業系統的基本介紹與回顧，與硬體相關的介面需求，然後對於作業系統的各項組成元件模組，如行程管理，CPU排班'),
+(2, 1002, '微處理機', 3, '資訊三乙', '(五)3', '資電402', 80, 10, '9002', '本課程介紹微處理機系統結構, 記憶體元件, 記憶體介面, I/O設備, I/O介面, 以及I/O方法之Polling I/O, Interrupt I/O, DMA I/O。'),
+(3, 1003, '網路程式設計', 2, '資訊三合', '(二)7', '資電104', 60, 38, '9003', '介紹各種主從式架構下網路程式設計及實作技巧'),
+(4, 1004, '軟體工程開發實務', 3, '資訊三合', '(三)6', '工319', 60, 3, '9004', '介紹軟體工程基本的原則與觀念。學生修習此課後可以學習到軟體工程紀律、專案管理、需求、設計、測試、版本管理及軟體維護等基本觀念。學生可以透過操作軟體工程工具的方式學習這些概念。'),
+(5, 1005, '實用職場英文', 2, '資訊三乙', '(四)6', '資電104', 62, 1, '9005', '「實用職場英文」以提升學生職場英語能力為主軸，採取互動及務實的教學方式，著重培養學生在職場所須之溝通技巧。課程架構涵蓋口說與寫作訓練，除了求職所需之英語能力外，亦針對進入職場後所需之英語溝通技巧。'),
+(6, 1006, '3D動畫實作與歷史建物永續I', 2, '科技知識原理與趨勢浪潮', '(一)11', '土302', 40, 1, '9006', '虛擬實境共分成三大類，分別是影像式虛擬實境、幾何式虛擬實境與混合式虛擬實境，而不論是哪種類型，3D模型的建立都是最基礎的過程。本課程將透過對知名或歷史建物的3D描繪，去學習繪製的技術及瞭解建物落成的歷'),
+(7, 1007, '消費權益與生活素養', 2, '人文藝術與社會經典教育', '(四)11', '商201', 76, 3, '9007', '介紹消者保護理念與法律思維，並以實例研討方式，說明消費權益保護的相關議題，學習日常生活中所涉網路交易、電視購物、不實廣告、定型契約、產品責任、消費申訴等問題處理知能。同時加強同學們對本課程與各種社會脈'),
+(8, 1001, '作業系統', 3, '資訊三乙', '(一)4', '資電102', 80, 4, '9001', '本課程旨在介紹電腦作業系統的內部組成與運作原理，也就是電腦系統內部管理的相關知識。課程內容包括作業系統的基本介紹與回顧，與硬體相關的介面需求，然後對於作業系統的各項組成元件模組，如行程管理，CPU排班'),
+(9, 1002, '微處理機', 3, '資訊三乙', '(五)4', '資電402', 80, 10, '9002', '本課程介紹微處理機系統結構, 記憶體元件, 記憶體介面, I/O設備, I/O介面, 以及I/O方法之Polling I/O, Interrupt I/O, DMA I/O。'),
+(10, 1003, '網路程式設計', 2, '資訊三合', '(二)8', '資電104', 60, 38, '9003', '介紹各種主從式架構下網路程式設計及實作技巧'),
+(11, 1004, '軟體工程開發實務', 3, '資訊三合', '(三)7', '工319', 60, 3, '9004', '介紹軟體工程基本的原則與觀念。學生修習此課後可以學習到軟體工程紀律、專案管理、需求、設計、測試、版本管理及軟體維護等基本觀念。學生可以透過操作軟體工程工具的方式學習這些概念。'),
+(12, 1004, '軟體工程開發實務', 3, '資訊三合', '(三)8', '工319', 60, 3, '9004', '介紹軟體工程基本的原則與觀念。學生修習此課後可以學習到軟體工程紀律、專案管理、需求、設計、測試、版本管理及軟體維護等基本觀念。學生可以透過操作軟體工程工具的方式學習這些概念。'),
+(13, 1005, '實用職場英文', 2, '資訊三乙', '(四)7', '資電104', 62, 1, '9005', '「實用職場英文」以提升學生職場英語能力為主軸，採取互動及務實的教學方式，著重培養學生在職場所須之溝通技巧。課程架構涵蓋口說與寫作訓練，除了求職所需之英語能力外，亦針對進入職場後所需之英語溝通技巧。'),
+(14, 1006, '3D動畫實作與歷史建物永續I', 2, '科技知識原理與趨勢浪潮', '(一)12', '土302', 40, 1, '9006', '虛擬實境共分成三大類，分別是影像式虛擬實境、幾何式虛擬實境與混合式虛擬實境，而不論是哪種類型，3D模型的建立都是最基礎的過程。本課程將透過對知名或歷史建物的3D描繪，去學習繪製的技術及瞭解建物落成的歷'),
+(15, 1007, '消費權益與生活素養', 2, '人文藝術與社會經典教育', '(四)12', '商201', 76, 3, '9007', '介紹消者保護理念與法律思維，並以實例研討方式，說明消費權益保護的相關議題，學習日常生活中所涉網路交易、電視購物、不實廣告、定型契約、產品責任、消費申訴等問題處理知能。同時加強同學們對本課程與各種社會脈'),
+(16, 2996, '韓文(一)', 2, '應用外語選修', '(一)1', '語206', 68, 5, '9008', ''),
+(17, 2996, '韓文(一)', 2, '應用外語選修', '(一)2', '語206', 68, 5, '9008', ''),
+(18, 876, '資訊安全與生活', 2, '創能學院綜合班', '(一)3', '圖213', 60, 2, '9009', ''),
+(19, 876, '資訊安全與生活', 2, '創能學院綜合班', '(一)4', '圖213', 60, 2, '9009', '');
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `focused_course`
+--
+
+CREATE TABLE `focused_course` (
+  `course_code` char(4) NOT NULL,
+  `student_id` char(8) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- 傾印資料表的資料 `focused_course`
+--
+
+INSERT INTO `focused_course` (`course_code`, `student_id`) VALUES
+('1002', 'D1018734'),
+('1001', 'D1018734'),
+('876', 'D1018734');
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `selected_course`
+--
+
+CREATE TABLE `selected_course` (
+  `id` int(11) NOT NULL,
+  `time` varchar(8) NOT NULL,
+  `course_code` char(4) NOT NULL,
+  `student_id` char(8) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- 傾印資料表的資料 `selected_course`
+--
+
+INSERT INTO `selected_course` (`id`, `time`, `course_code`, `student_id`) VALUES
+(2, '(二)7', '1003', 'D1018734'),
+(3, '(三)6', '1004', 'D1018734'),
+(5, '(五)3', '1002', 'D1018734'),
+(8, '(五)4', '1002', 'D1018734'),
+(10, '(二)8', '1003', 'D1018734'),
+(11, '(三)7', '1004', 'D1018734'),
+(12, '(三)8', '1004', 'D1018734'),
+(42, '(一)3', '1001', 'D1018734'),
+(43, '(一)4', '1001', 'D1018734');
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `students_account`
+--
+
+CREATE TABLE `students_account` (
+  `nid` char(8) NOT NULL,
+  `pwd` varchar(70) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- 傾印資料表的資料 `students_account`
+--
+
+INSERT INTO `students_account` (`nid`, `pwd`) VALUES
+('D1012337', '$2a$08$62Y2jOnQTTHcIkuV7jOa4uT28iEi5uFzlV9iBuWJfwqPYdbR9xoIu'),
+('D0123456', '$2y$10$gBCQmzPHkV2mbkPeElGqReY8CzJ.UK6DuRBzfYvF0YihhNhRSLfzu'),
+('D1234567', '$2y$10$bYS.UNByzsSvBBpul1CZIOp/c8NIJajBy0P4qcd8cPJlVKP.3HPmy'),
+('D9876543', '$2y$10$j11B978ttJSlqYNBlQ2pMO45j/Fi0bZH8dDa8mli3EW0j7.eEcnoS'),
+('D1018734', '$2y$10$j11B978ttJSlqYNBlQ2pMO45j/Fi0bZH8dDa8mli3EW0j7.eEcnoS');
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `students_infomation`
+--
+
+CREATE TABLE `students_infomation` (
+  `id` char(8) NOT NULL,
+  `name` varchar(30) NOT NULL,
+  `grade` int(1) NOT NULL,
+  `class` varchar(4) NOT NULL,
+  `email` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `teacher_information`
+--
+
+CREATE TABLE `teacher_information` (
+  `id` char(8) NOT NULL,
+  `name` varchar(20) NOT NULL,
+  `email` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- 傾印資料表的資料 `teacher_information`
+--
+
+INSERT INTO `teacher_information` (`id`, `name`, `email`) VALUES
+('9001', 'AAA', 'AAA@gmail.com'),
+('9002', 'BBB', 'BBB@gmail.com'),
+('9003', 'CCC', 'CCC@gmail.com'),
+('9004', 'DDD', 'DDD@gmail.com'),
+('9005', 'EEE', 'EEE@gmail.com'),
+('9006', 'FFF', 'FFF@gmail.com'),
+('9007', 'GGG', 'GGG@gmail.com'),
+('9008', 'ZZZ', ''),
+('9009', 'JJJ', '');
+
+--
+-- 已傾印資料表的索引
+--
+
+--
+-- 資料表索引 `all_course`
+--
+ALTER TABLE `all_course`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- 資料表索引 `selected_course`
+--
+ALTER TABLE `selected_course`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- 資料表索引 `students_infomation`
+--
+ALTER TABLE `students_infomation`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- 資料表索引 `teacher_information`
+--
+ALTER TABLE `teacher_information`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- 在傾印的資料表使用自動遞增(AUTO_INCREMENT)
+--
+
+--
+-- 使用資料表自動遞增(AUTO_INCREMENT) `all_course`
+--
+ALTER TABLE `all_course`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+
+--
+-- 使用資料表自動遞增(AUTO_INCREMENT) `selected_course`
+--
+ALTER TABLE `selected_course`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=44;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
